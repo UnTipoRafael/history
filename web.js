fc7edd1e98a358c464c1707cf6024efc888b0756
Fernando <rafelox@cubcode.com>
2013-03-04 10:33:18 -0500

    io

--- web.js
+++ web.js
@@ -2,12 +2,13 @@
 /**
  * Module dependencies.
  */
-
+var port = 2001;
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
+  , sio = require('socket.io')
   , mysql = require('mysql');
 
 var app = express();
@@ -30,7 +31,7 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
-/**/
+
 
 function login(req, res, next){
   if(req.session.user){
@@ -50,6 +51,7 @@ app.post('/autenticar', routes.autenticar);
 app.post('/registro', routes.registro);
 app.get('/registro/fb', routes.registroFB);
 app.get('/salir', routes.salir);
+
 
 
 
@@ -76,10 +78,19 @@ app.post('fb-login',function(req,res){
 */
 
 
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  //console.log("Express server listening on port " + app.get('port'));
+ 
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
 });
 
 
+var io = require('socket.io').listen(server);
+
+
+/**/
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
